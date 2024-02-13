835891e304c9bd71dbfc97b817236c4c9575f859
Squiddymur <gibbrianarifin@gmail.com>
2024-02-13 08:38:35 +0700

    Update dashboard.dart

--- lib/Admin/Dashboard/dashboard.dart
+++ lib/Admin/Dashboard/dashboard.dart
@@ -64,7 +64,7 @@ class Dashboard extends StatelessWidget {
                           decoration: BoxDecoration(boxShadow: [
                             BoxShadow(
                                 color: Color.fromRGBO(
-                                    156, 156, 156, 0.28999999165534973),
+                                    156, 156, 156, 0.28999999165534972),
                                 offset: Offset(0, 0),
                                 blurRadius: 55.5)
                           ]),

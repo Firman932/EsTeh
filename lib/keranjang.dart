0126d8bae394f0e2e4d013a962d1845ace7da62e
Firman932 <159208713+Firman932@users.noreply.github.com>
2024-03-07 15:15:41 +0700

    req

--- lib/keranjang.dart
+++ lib/keranjang.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lji/styles/dialog.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -76,138 +78,167 @@ class _KeranjangState extends State<Keranjang> {
           },
         ),
       ),
-      body: ListView(
-        children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 28, 20, 20),
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Color(0xffffffff),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(20, 28, 20, 20),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Color(0xffffffff),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(5, 0, 5, 20),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context)
+                            .size
+                            .width, // Sesuaikan tinggi dengan lebar untuk membuat gambar persegi
+                        child: AspectRatio(
+                          aspectRatio:
+                              1, // Memastikan gambar tetap berbentuk persegi
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              widget.produkData["gambar_produk"],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 20),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.only(top: 3),
+                          child: Text(
+                            widget.produkData["nama_produk"],
+                            style: GoogleFonts.poppins(
+                              fontSize: 27,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xff030303),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.only(
+                              left: 2 + MediaQuery.of(context).padding.left),
+                          child: Text(
+                            widget.produkData["variasi_rasa"],
+                            style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xff000000),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    width: 800,
+                    height: 30,
+                    child: Text(
+                      'Rp ${widget.produkData["harga_produk"]}',
+                      style: GoogleFonts.poppins(
+                        fontSize: 21,
+                        fontWeight: FontWeight.w600,
+                        height: 1.5,
+                        color: Color(0xff49a013),
+                      ),
+                    ),
+                  ),
+                ],
+              ), //kedua
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ]),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: Color(0x499c9c9c),
+            offset: Offset(0, 0),
+            blurRadius: 2,
+          ),
+        ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+          child: BottomAppBar(
+            surfaceTintColor: Colors.white,
+            height: 70,
+            elevation: 1,
+            notchMargin: 8,
+            shape: CircularNotchedRectangle(),
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(5, 0, 5, 20),
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context)
-                          .size
-                          .width, // Sesuaikan tinggi dengan lebar untuk membuat gambar persegi
-                      child: AspectRatio(
-                        aspectRatio:
-                            1, // Memastikan gambar tetap berbentuk persegi
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                            widget.produkData["gambar_produk"],
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+                ElevatedButton(
+                    onPressed: () {
+                      // Tambahkan logika untuk memasukkan produk ke keranjang
+                      tambahkanKeKeranjang();
+                      print(
+                          'Menambahkan ${widget.produkData["nama_produk"]} ke keranjang');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.minPositive, 50),
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                    )),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Tambahkan logika untuk membeli langsung
+                      print(
+                          'Membeli langsung ${widget.produkData["nama_produk"]}');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.minPositive, 50),
+                      backgroundColor: Color(0xff4fb60e),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    child: Text(
+                      'Beli Langsung',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xffffffff),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 0, 20),
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.only(top: 3),
-                        child: Text(
-                          widget.produkData["nama_produk"],
-                          style: GoogleFonts.poppins(
-                            fontSize: 27,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xff030303),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.only(
-                            left: 2 + MediaQuery.of(context).padding.left),
-                        child: Text(
-                          widget.produkData["variasi_rasa"],
-                          style: GoogleFonts.poppins(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xff000000),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  width: 800,
-                  height: 30,
-                  child: Text(
-                    'Rp ${widget.produkData["harga_produk"]}',
-                    style: GoogleFonts.poppins(
-                      fontSize: 21,
-                      fontWeight: FontWeight.w600,
-                      height: 1.5,
-                      color: Color(0xff49a013),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {
-                          // Tambahkan logika untuk memasukkan produk ke keranjang
-                          tambahkanKeKeranjang();
-                          print(
-                              'Menambahkan ${widget.produkData["nama_produk"]} ke keranjang');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(double.minPositive, 50),
-                          backgroundColor: Colors.blue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.add_shopping_cart,
-                          color: Colors.white,
-                        )),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Tambahkan logika untuk membeli langsung
-                        print(
-                            'Membeli langsung ${widget.produkData["nama_produk"]}');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.minPositive, 50),
-                        backgroundColor: Color(0xff4fb60e),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                      ),
-                      child: Text(
-                        'Beli Langsung',
-                        style: GoogleFonts.poppins(
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }

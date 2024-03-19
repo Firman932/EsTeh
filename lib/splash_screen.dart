d68d6cca2ae5eb77b16c66e9fea1692cbd694cc5
Squiddymur <gibbrianarifin@gmail.com>
2024-03-19 11:35:58 +0700

    logout

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:connectivity_plus/connectivity_plus.dart'; // Import package connectivity
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lji/Admin/Dashboard/dashboard.dart';
@@ -15,27 +17,8 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   var auth = FirebaseAuth.instance;
   var isLogin = false;
-
-  // Function to check if there is internet connection
-  Future<bool> checkInternetConnection() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
-  }
-
-  checkIfLogin() async {
-    auth.authStateChanges().listen((User? user) {
-      if (user != null && mounted) {
-        setState(() {
-          isLogin = true;
-        });
-        navigateToNextScreen(
-            user); // Panggil fungsi navigateToNextScreen dengan user yang diterima
-      } else {
-        navigateToNextScreen(
-            null); // Panggil fungsi navigateToNextScreen dengan null jika user null atau belum login
-      }
-    });
-  }
+  var internetDialogShown = false;
+  late Stream<User?> _authStateStream;
 
   @override
   void initState() {
@@ -43,44 +26,64 @@ class _SplashScreenState extends State<SplashScreen> {
     initializeApp();
   }
 
-  // Function to initialize the app and handle navigation
+  @override
+  void dispose() {
+    _authStateStream.drain();
+    super.dispose();
+  }
+
+  Future<bool> checkInternetConnection() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    return connectivityResult != ConnectivityResult.none;
+  }
+
+  void checkIfLogin() async {
+    _authStateStream = auth.authStateChanges();
+    _authStateStream.listen((User? user) {
+      if (mounted) {
+        setState(() {
+          isLogin = user != null;
+        });
+        navigateToNextScreen(user);
+      }
+    });
+  }
+
   void initializeApp() async {
     bool isConnected = await checkInternetConnection();
     if (!isConnected) {
-      // If there's no internet connection, we wait for it
       await waitForInternetConnection();
     } else {
-      // If there's internet, continue with the app initialization
       checkIfLogin();
     }
   }
 
-  // Function to wait for internet connection
-  waitForInternetConnection() async {
-    showDialog(
-      context: context,
-      builder: (context) => LostConnect(
-        title: "Tidak Ada Koneksi Internet",
-        content:
-            "Maaf, sepertinya anda tidak terhubung ke internet saat ini. Pastikan anda terhubung ke jaringan wifi atau data saluler",
-      ),
-    );
+  Future<void> waitForInternetConnection() async {
+    if (!internetDialogShown) {
+      internetDialogShown = true;
+      showDialog(
+        context: context,
+        builder: (context) => LostConnect(
+          title: "Tidak Ada Koneksi Internet",
+          content:
+              "Maaf, sepertinya anda tidak terhubung ke internet saat ini. Pastikan anda terhubung ke jaringan wifi atau data saluler",
+        ),
+      );
+    }
 
-    // Loop until internet connection is established
     while (true) {
       await Future.delayed(Duration(seconds: 3)); // Check every second
       bool isConnected = await checkInternetConnection();
       if (isConnected) {
         Navigator.pop(context); // Close the dialog
-        break; // Exit the loop if internet connection is established
+        internetDialogShown = false;
+        break;
       }
     }
 
-    // Continue with the app initialization after internet connection is established
     initializeApp();
   }
 
-  // Function to navigate to the next screen based on user authentication
   void navigateToNextScreen(User? user) async {
     if (user != null && isLogin) {
       DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
@@ -90,22 +93,30 @@ class _SplashScreenState extends State<SplashScreen> {
 
       String role = userSnapshot['role'];
       if (role == 'admin') {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => Dashboard()),
-        );
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => MenuUser()),
-        );
-      }
-    } else {
-      // Clear all existing routes and navigate to the SignScreen
+        Timer(Duration(seconds: 3), () {
+      // Navigasi ke halaman berikutnya
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => SignScreen()),
+        MaterialPageRoute(builder: (context) => Dashboard()), // Ganti NextPage dengan nama halaman selanjutnya
       );
+    });
+      } else {
+        Timer(Duration(seconds: 3), () {
+      // Navigasi ke halaman berikutnya
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MenuUser()), // Ganti NextPage dengan nama halaman selanjutnya
+      );
+    });
+      }
+    } else {
+      Timer(Duration(seconds: 3), () {
+      // Navigasi ke halaman berikutnya
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SignScreen()), // Ganti NextPage dengan nama halaman selanjutnya
+      );
+    });
     }
   }
 
@@ -115,25 +126,21 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment(-1, -1.031),
-              end: Alignment(1, 1),
-              colors: <Color>[Color(0xffa5cd14), Color(0xff55bc15)],
-              stops: <double>[0.285, 0.837],
-            ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(-1, -1.031),
+            end: Alignment(1, 1),
+            colors: <Color>[Color(0xffa5cd14), Color(0xff55bc15)],
+            stops: <double>[0.285, 0.837],
           ),
-          child: Center(
-            child: Container(
-              width: MediaQuery.of(context).size.width * .8,
-              height: MediaQuery.of(context).size.height * .4,
-              child: Image.asset(
-                "assets/Logoes.png",
-                fit: BoxFit.contain,
-              ),
+        ),
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * .8,
+            height: MediaQuery.of(context).size.height * .4,
+            child: Image.asset(
+              "assets/Logoes.png",
+              fit: BoxFit.contain,
             ),
           ),
         ),
